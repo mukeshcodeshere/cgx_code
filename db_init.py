87bd5f8b7eb06a0dd44698679d5c01af3f97939e
XiaoTian Zhou <xtzhou@berkeley.edu>
2024-04-02 17:15:15 -0700

    first commit

--- db_init.py
+++ db_init.py
@@ -1,17 +1,18 @@
 # db_init.py
 from app import create_app, db
 from app.models import CurrencyPair
+from forex_python.converter import CurrencyRates, RatesNotAvailableError
 
 app = create_app()
-
+c = CurrencyRates()
 with app.app_context():
     db.create_all()
 
     # Sample currency pairs
-    btc_usdt = CurrencyPair(base_currency='bitcoin', quote_currency='usdt', exchange_rate=50000.0)
-    eth_usdt = CurrencyPair(base_currency='ethereum', quote_currency='usdt', exchange_rate=2000.0)
-    btc_usdc = CurrencyPair(base_currency='bitcoin', quote_currency='usdc', exchange_rate=50000.0)
-    eth_usdc = CurrencyPair(base_currency='ethereum', quote_currency='usdc', exchange_rate=2000.0)
+    btc_usdt = CurrencyPair(base_currency='EURO', quote_currency='USDT', exchange_rate=c.get_rate('USD', 'EUR'))
+    eth_usdt = CurrencyPair(base_currency='GBP', quote_currency='USDT', exchange_rate=c.get_rate('USD', 'GBP'))
+    btc_usdc = CurrencyPair(base_currency='YEN', quote_currency='USDT', exchange_rate=c.get_rate('USD', 'JPY'))
+    eth_usdc = CurrencyPair(base_currency='YUAN', quote_currency='USDT', exchange_rate=c.get_rate('USD', 'CNY'))
 
     # Add the currency pairs to the session
     db.session.add(btc_usdt)
