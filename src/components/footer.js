0737a92ed2797f3521f7a3f0ac3d1e247ebe7c35
Mukeshwaran Baskaran <mukesh96official@gmail.com>
2024-04-23 11:34:29 -0700

    changing the world

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,10 +5,10 @@ export const Footer = () => {
     <footer class="relative text-center text-black-400 lg:text-center md:text-center sm:text-center">
         <div className="my-4">
           <h3 className="footer-content font-semibold text-gray-300">
-            Made with{" "}❤️{" "}
+            Demo by
             <a
               class="link"
-              href="https://github.com/dreamspartan"
+              href="https://en.wikipedia.org/wiki/Bitcoin"
               className="hover:underline hover:text-blue-400"
             >
               Curiosity Global{" "}
@@ -16,7 +16,7 @@ export const Footer = () => {
           </h3>
         </div>
         <div className='flex absolute -mt-3 items-center'>
-            <h3 className='text-[8px] text-gray-300'>Data Provided By CoinGeckoAPI</h3>
+            <h3 className='text-[8px] text-gray-300'>Data streaming via CoinGeckoAPI</h3>
         </div>
       </footer>
   )
